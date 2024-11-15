4c990c408901c84158fc0a00c0924ec6b2cceaed
snobbar jaffer <83435305+SnobbarJaffer-123@users.noreply.github.com>
2024-11-15 08:25:50 +0530

    Add files via upload

---
+++ FloatToInt.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class FloatToInt{
+public static void main(String []args )
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a floating point number");
+float num=sc.nextFloat();
+int inum=(int)num;
+System.out.println("flaot value "+num+" to integer:"+ inum);
+}}
